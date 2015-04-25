e687c4a6767ef397a18379a25149b7620db97228
Liu Jiyang <2411mail@gmail.com>
2015-04-25 16:54:29 +0800

    add customized layout

--- router.js
+++ router.js
@@ -1,3 +1,13 @@
-Router.route('/register', function () {
+Router.configure({
+  // we use the  appBody template to define the layout for the entire app
+  layoutTemplate: 'appLayout'
+});
+
+Router.route('/signup', function () {
   this.render('signup');
+  this.layout("loginLayout");
+});
+
+Router.route('/', function () {
+  this.render('showPlayer');
 });
